14a3fb1bfcda16f6327654d067fa5f92d8c13348
Nicolas Manzato <nicolas.manzato@globant.com>
2019-01-22 12:11:10 -0300

    base.scss added and integrated with components

--- src/app/global-data/global-data.component.ts
+++ src/app/global-data/global-data.component.ts
@@ -5,7 +5,7 @@ import { GlobalData } from '../model/globalData';
 @Component({
   selector: 'app-global-data',
   templateUrl: './global-data.component.html',
-  styleUrls: ['./global-data.component.css']
+  styleUrls: ['./global-data.component.scss']
 })
 export class GlobalDataComponent implements OnInit {
 
