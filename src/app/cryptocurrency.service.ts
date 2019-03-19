ecaa039cb9bb5caaa64da0125ce3d6628e478092
nicomanzato <nicomanzato@gmail.com>
2019-03-18 21:37:52 -0300

    server configuration added to package.json

--- src/app/cryptocurrency.service.ts
+++ src/app/cryptocurrency.service.ts
@@ -1,27 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Cryptocurrency, CryptocurrencyAdapter } from './model/cryptocurrency';
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap, filter } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Cryptocurrency, CryptocurrencyAdapter } from "./model/cryptocurrency";
+import { Observable, of } from "rxjs";
+import { catchError, map, tap, filter } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CryptocurrencyService {
-
-  private cryptocurrencyUrl = 'http://10.160.11.56:8080/cryptocurrency';
+  private cryptocurrencyUrl = "http://localhost:8080/cryptocurrency";
 
   constructor(
     private http: HttpClient,
     private adapter: CryptocurrencyAdapter
-  ) { }
+  ) {}
 
   getCryptocurrencies(): Observable<Cryptocurrency[]> {
-    return this.http.get<any[]>(this.cryptocurrencyUrl)
-      .pipe(
-        map((data: any[]) => data.map(item => this.adapter.adapt(item))),
-        catchError(this.handleError('getCryptocurrencies', []))
-      );
+    return this.http.get<any[]>(this.cryptocurrencyUrl).pipe(
+      map((data: any[]) => data.map(item => this.adapter.adapt(item))),
+      catchError(this.handleError("getCryptocurrencies", []))
+    );
   }
 
   getCryptocurrency(id: string): Observable<any> {
@@ -29,14 +27,13 @@ export class CryptocurrencyService {
     return this.http.get<any>(url).pipe(
       map((item: any) => this.adapter.adapt(item)),
       catchError(this.handleError(`getCryptocurrency id=${id}`))
-    )
+    );
   }
 
-  private handleError<T> (operation = 'operation', result?: T) {
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       return of(result as T);
     };
   }
-
 }
--- src/app/global-data.service.ts
+++ src/app/global-data.service.ts
@@ -1,30 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { GlobalData, GlobalDataAdapter } from './model/globalData';
-import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { GlobalData, GlobalDataAdapter } from "./model/globalData";
+import { Observable, of } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GlobalDataService {
+  private globalDataUrl = "http://localhost:8080/global_data";
 
-  private globalDataUrl = 'http://10.160.11.56:8080/global_data';
-
-  constructor(
-    private http: HttpClient,
-    private adapter: GlobalDataAdapter
-  ) { }
+  constructor(private http: HttpClient, private adapter: GlobalDataAdapter) {}
 
   getGlobalData(): Observable<any> {
-    return this.http.get<any>(this.globalDataUrl)
-      .pipe(
-        map((item: any) => this.adapter.adapt(item)),
-        catchError(this.handleError('getGlobalData', []))
-      );
+    return this.http.get<any>(this.globalDataUrl).pipe(
+      map((item: any) => this.adapter.adapt(item)),
+      catchError(this.handleError("getGlobalData", []))
+    );
   }
 
-  private handleError<T> (operation = 'operation', result?: T) {
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
       return of(result as T);
