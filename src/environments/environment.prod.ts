fd26cb4a99265a8922ad1db239106cf54531c298
Nicolas Manzato <nicolasmanzato@MacBook-Pro-de-Nicolas.local>
2019-04-19 14:19:24 -0300

    deploy config added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
   ENDPOINTS: {
-    CRYPTOCURRENCY_API: "http://localhost:8080/cryptocurrency",
-    GLOBAL_DATA_API: "http://localhost:8080/global_data"
+    CRYPTOCURRENCY_API:
+      "https://angular-cryptowatcher.herokuapp.com:8080/cryptocurrency",
+    GLOBAL_DATA_API:
+      "https://angular-cryptowatcher.herokuapp.com:8080/global_data"
   }
 };
